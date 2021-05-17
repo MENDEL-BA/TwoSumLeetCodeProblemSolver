311f1ef98cc812d6309d0aa19290609850032e7d
Mendel Ba <>
2021-05-17 12:14:56 +0000

    redifine function and optimize this

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 var twoSum = function(nums, target) {
     
     var sum = 0;
-    var tabIndexOfSum = [];
+    var tabIndexOfSum =[];
     
     if(nums.length === 0 ) {
         console.log("le tableau est vide");
@@ -15,7 +15,7 @@ var twoSum = function(nums, target) {
     for(var i=0; i < nums.length; i++) {
         
         sum += nums[i];
-        tabIndexOfSum.push(nums[i]);
+        tabIndexOfSum.push(i);
         if(sum === target ) {
             
             console.log("Im in ", sum, target, tabIndexOfSum)
@@ -23,4 +23,19 @@ var twoSum = function(nums, target) {
     }
 }; 
 
-twoSum([2,7,11,15],9);
+
+
+var twoSumRef = function(nums, target) {
+  numsIndexes = {};
+
+  for (let i = 0; i < nums.length; i += 1) {
+    let currentDifference = target - nums[i];
+
+    if (numsIndexes[currentDifference] !== undefined && numsIndexes[currentDifference] !== i) {
+      return [i, numsIndexes[currentDifference]];
+    } else {
+      numsIndexes[nums[i]] = i;
+    }
+  }
+};
+twoSumRef([2,7,11,15],9);
